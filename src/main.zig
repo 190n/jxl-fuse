e532ec51a089b7ebe334981a03c24017ea7c78cc
Ben Grant <benjamin.j.grant@gmail.com>
2023-06-30 16:21:54 -0700

    Move Buffers outside jxlToJpeg

--- src/main.zig
+++ src/main.zig
@@ -1,54 +1,58 @@
 const std = @import("std");
 const libjxl = @import("./libjxl.zig");
 
+/// helper struct to manage shuffling data to/from libjxl
+const Buffers = struct {
+    full_buffer: []u8,
+    used_buffer: []const u8,
+    last_chunk: []u8,
+    allocator: std.mem.Allocator,
+
+    pub fn init(ally: std.mem.Allocator) Buffers {
+        const buf = @as([*]u8, undefined)[0..0];
+        return .{
+            .full_buffer = buf,
+            .used_buffer = buf[0..0],
+            .last_chunk = buf,
+            .allocator = ally,
+        };
+    }
+
+    pub fn deinit(self: *Buffers) void {
+        self.allocator.free(self.full_buffer);
+        self.* = undefined;
+    }
+
+    pub fn toOwned(self: *Buffers) ![]u8 {
+        const slice = try self.allocator.realloc(self.full_buffer, self.used_buffer.len);
+        // freeing slice of length 0 is no-op
+        self.full_buffer = @as([*]u8, undefined)[0..0];
+        return slice;
+    }
+
+    pub fn release(self: *Buffers, dec: *libjxl.Decoder) void {
+        const unwritten_bytes = dec.releaseJpegBuffer();
+        const bytes_in_last_chunk = self.last_chunk.len - unwritten_bytes;
+        const before_last_chunk = self.full_buffer.len - self.last_chunk.len;
+        self.used_buffer = self.full_buffer[0..(before_last_chunk + bytes_in_last_chunk)];
+    }
+
+    /// returns the chunk that can be provided to libjxl
+    pub fn provideMoreRoom(self: *Buffers, dec: *libjxl.Decoder, size_hint: ?usize) ![]u8 {
+        self.release(dec);
+        std.log.info("realloc from {}", .{self.full_buffer.len});
+        self.full_buffer = try self.allocator.realloc(
+            self.full_buffer,
+            @max(size_hint orelse 4096, 2 * self.full_buffer.len),
+        );
+        std.log.info("to {}", .{self.full_buffer.len});
+        self.used_buffer.ptr = self.full_buffer.ptr;
+        self.last_chunk = self.full_buffer[self.used_buffer.len..];
+        return self.last_chunk;
+    }
+};
+
 fn jxlToJpeg(jxl_buffer: []const u8, allocator: std.mem.Allocator) ![]u8 {
-    const Buffers = struct {
-        full_buffer: []u8,
-        used_buffer: []const u8,
-        last_chunk: []u8,
-        allocator: std.mem.Allocator,
-
-        const Buffers = @This();
-
-        pub fn init(ally: std.mem.Allocator) Buffers {
-            const buf = @as([*]u8, undefined)[0..0];
-            return .{
-                .full_buffer = buf,
-                .used_buffer = buf[0..0],
-                .last_chunk = buf,
-                .allocator = ally,
-            };
-        }
-
-        pub fn deinit(self: *Buffers) void {
-            self.allocator.free(self.full_buffer);
-            self.* = undefined;
-        }
-
-        pub fn toOwned(self: *Buffers) ![]u8 {
-            const slice = try self.allocator.realloc(self.full_buffer, self.used_buffer.len);
-            // freeing slice of length 0 is no-op
-            self.full_buffer = @as([*]u8, undefined)[0..0];
-            return slice;
-        }
-
-        pub fn release(self: *Buffers, dec: *libjxl.Decoder) void {
-            const unwritten_bytes = dec.releaseJpegBuffer();
-            const bytes_in_last_chunk = self.last_chunk.len - unwritten_bytes;
-            const before_last_chunk = self.full_buffer.len - self.last_chunk.len;
-            self.used_buffer = self.full_buffer[0..(before_last_chunk + bytes_in_last_chunk)];
-        }
-
-        /// returns the chunk that can be provided to libjxl
-        pub fn provideMoreRoom(self: *Buffers, dec: *libjxl.Decoder) ![]u8 {
-            self.release(dec);
-            self.full_buffer = try self.allocator.realloc(self.full_buffer, @max(4096, 2 * self.full_buffer.len));
-            self.used_buffer.ptr = self.full_buffer.ptr;
-            self.last_chunk = self.full_buffer[self.used_buffer.len..];
-            return self.last_chunk;
-        }
-    };
-
     const signature_result = libjxl.signatureCheck(jxl_buffer);
     if (signature_result != .Codestream and signature_result != .Container) {
         return error.NotJxlFile;
@@ -65,7 +69,7 @@ fn jxlToJpeg(jxl_buffer: []const u8, allocator: std.mem.Allocator) ![]u8 {
     var bufs = Buffers.init(allocator);
     defer bufs.deinit();
 
-    try decoder.setJpegBuffer(try bufs.provideMoreRoom(&decoder));
+    try decoder.setJpegBuffer(try bufs.provideMoreRoom(&decoder, jxl_buffer.len * 11 / 8));
 
     while (true) {
         const status = try decoder.processInput();
@@ -74,7 +78,7 @@ fn jxlToJpeg(jxl_buffer: []const u8, allocator: std.mem.Allocator) ![]u8 {
             .need_image_out_buffer => return error.NotRecompressedJpeg,
             .jpeg_reconstruction => {},
             .jpeg_need_more_output => {
-                const buf = try bufs.provideMoreRoom(&decoder);
+                const buf = try bufs.provideMoreRoom(&decoder, null);
                 try decoder.setJpegBuffer(buf);
             },
             .full_image => {
